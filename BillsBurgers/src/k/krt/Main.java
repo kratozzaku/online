527d1ac11301bb7c0e951231845706bf6116d579
kratozzaku <kratozz@gmail.com>
2015-12-10 01:55:40 +0200

    Change-Id: I82a3799364bca16c8306c4cca3b1ae6244b2817e

--- BillsBurgers/src/k/krt/Main.java
+++ BillsBurgers/src/k/krt/Main.java
@@ -3,7 +3,22 @@ package k.krt;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Ingredient tomatoes = new Ingredient("tomatoes");
+		Ingredient other = new Ingredient("other");
+				
+		Hamburger firstHamburger = new Hamburger("Bread", "Chicken", 20.0);
+		
+		
+		firstHamburger.addIngredient(tomatoes);
+		firstHamburger.addIngredient(other);
+		
+		firstHamburger.addIngredient(tomatoes);
+		firstHamburger.addIngredient(other);
+		firstHamburger.addIngredient(tomatoes);
+		
+		firstHamburger.displaySummary();
+		
 
 	}
 
