416328003fcdfd3369fe460ebadb005fcfce52bc
kratozzaku <kratozz@gmail.com>
2015-12-16 21:44:18 +0200

    Change-Id: I3fec13a34fe7955b57dfb9c0349fdd416b22dabf

--- Arrays/src/krt/com/Main.java
+++ Arrays/src/krt/com/Main.java
@@ -3,7 +3,18 @@ package krt.com;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		int[] myIntArray = new int[10];
+		
+		myIntArray[0] = 10;
+		myIntArray[1] = 10;
+		myIntArray[5] = 50;
+		
+		
+		
+		
+		
 
 	}
 
