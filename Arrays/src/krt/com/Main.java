df4d4b55a0ae17829ad5de8d0ef9d6ac66b74cc4
kratozzaku <kratozz@gmail.com>
2015-12-17 01:56:33 +0200

    Change-Id: Ife6512f6e3cfe551ec1def66d837001589ffec74

--- Arrays/src/krt/com/Main.java
+++ Arrays/src/krt/com/Main.java
@@ -1,21 +1,92 @@
 package krt.com;
 
+
+import java.util.Scanner;
+
+
 public class Main {
+	
+	private static Scanner myScanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
+		int[] myArray = {9,7,15};
 		
-		int[] myIntArray = new int[10];
-		
-		myIntArray[0] = 10;
-		myIntArray[1] = 10;
-		myIntArray[5] = 50;
-		
-		
-		
+		//printArray(myArray);
+		printArray(sortIntegers(myArray));
 		
 		
 
 	}
+	
+	public static int[] getIntegers(int numbers){
+		int[] array = new int[numbers];
+		
+		System.out.println("Enter " + numbers + " integers.\r");
+		for (int i = 0; i < array.length; i++) {
+			array[i] = myScanner.nextInt();
+		}
+		
+		return array;
+		
+	}
+	
+	public static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Element[" + i + "]: " + array[i]);
+		}
+	}
+	
+	
+	
+	public static int[] sortIntegers(int[] array) {
+		int[] sortedArray = new int[array.length];
+		int temp = 0;
+				
+		for (int i = 0; i < array.length; i++) {
+			sortedArray[i] = array[i];
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	
+		return array;
+	}
+	
+	
+//	
+//	public static int[] sortIntegers(int[] array) {
+//		int[] sortedArray = new int[array.length];
+//		int max = array[0];
+//		
+//		
+//		for (int i = 0; i < array.length; i++) {
+//			for (int j = 0; j < array.length; j++) {
+//								
+//				if (array[i] > max && array[i] > array[j] ){
+//					max = array[i];
+//					sortedArray[i] = array[i];
+//				}else {
+//					max = array[j];
+//					sortedArray[i] = array[j];
+//				}
+//
+//				//System.out.println("max: " + max);
+//				
+//			}
+//		}
+//	
+//	
+//		return sortedArray;
+//	}
+//	
+	
 
 }
