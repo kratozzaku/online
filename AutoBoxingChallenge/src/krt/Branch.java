98ce9b326478ba480ca43d8a75ebc94bb610af9a
kratozzaku <kratozz@gmail.com>
2015-12-26 14:37:43 +0200

    Revert ""
    This reverts commit a851c3bb23f76eab8e3b77d6dd7ca82afa7949c5.

---
+++ AutoBoxingChallenge/src/krt/Branch.java
@@ -0,0 +1,20 @@
+package krt;
+
+import java.util.ArrayList;
+
+public class Branch {
+	
+	private String name;
+	private ArrayList<Customer> customers = new ArrayList<Customer>();
+	
+
+	public Branch(String name) {
+		super();
+		this.name = name;
+	}
+	
+	// TODO Add new customer, and initial transaction amount
+	// TODO Add additional transactions
+	
+	
+}
---
+++ AutoBoxingChallenge/src/krt/Main.java
@@ -0,0 +1,18 @@
+package krt;
+
+public class Main {
+	
+	private Bank theVault = new Bank ("The Valut"); 
+	
+	public static void main(String[] args) {
+		
+		// TODO Add menu
+		
+		
+		
+		
+		
+		
+	}
+
+}
