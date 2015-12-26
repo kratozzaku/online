50b2d2e0cafb09137ba526c2f5ca4c6d5f3e81e4
kratozzaku <kratozz@gmail.com>
2015-12-26 14:48:32 +0200

    Change-Id: I26c38ffb0a4354d304cd61d772863e720b4f6fa8

---
+++ AutoBoxingChallenge/src/krt/Bank.java
@@ -0,0 +1,20 @@
+package krt;
+
+import java.util.ArrayList;
+
+public class Bank {
+	
+	private String name;
+	private ArrayList<Branch> branches = new ArrayList<Branch>();
+
+	public Bank(String name) {
+		super();
+		this.name = name;
+	}
+	
+	// TODO Add new branch
+	// TODO Add customer to branch with initial transaction
+	// TODO Add transaction to existing customer
+	// TODO Show list of customers for a particular branch and optionally show their transactions
+	// TODO add validation check if exists
+}
